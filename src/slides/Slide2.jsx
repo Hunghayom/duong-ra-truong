9ac3a8d4b83ba0bf774ad0e0a73251aa77d0b702
Tạ Việt Hùng <124016028+Hunghayom@users.noreply.github.com>
2026-07-01 00:39:18 +0700

    Add files via upload

--- src/slides/Slide2.jsx
+++ src/slides/Slide2.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import SlideLayout from '../components/SlideLayout';
 
-export default function Slide2({ onNextSlide }) {
+export default function Slide2({ onNextSlide, onSkipIntro }) {
     return (
         <SlideLayout
             isTitleSlide={false}
             text={"Xin chào mọi người, mình là Tạ Việt Hùng. Mình là sinh viên trường ĐHCN - ĐHQGHN. \nNgày 5/7 tới sẽ là lễ tốt nghiệp của mình."}
             onNextSlide={onNextSlide}
+            onSkipIntro={onSkipIntro}
         >
             {/* 
         CHỖ TRỐNG ĐỂ CHÈN ẢNH SVG/ROUGH.JS 
--- src/slides/Slide3.jsx
+++ src/slides/Slide3.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import SlideLayout from '../components/SlideLayout';
 
-export default function Slide3({ onNextSlide }) {
+export default function Slide3({ onNextSlide, onSkipIntro }) {
     return (
         <SlideLayout
             isTitleSlide={false}
             text={"Để mà nói thì đây không phải nguyện vọng 1 của mình. \nNguyện vọng 1 của mình là các trường kiến trúc với mỹ thuật thôi, vì mình rất thích vẽ dù không giỏi lắm. \nNhưng, như một sự sắp đặt tình cờ, mình đã nhập học ở đây"}
             onNextSlide={onNextSlide}
+            onSkipIntro={onSkipIntro}
         >
             {/* 
         CHỖ TRỐNG ĐỂ CHÈN ẢNH SVG/ROUGH.JS 
